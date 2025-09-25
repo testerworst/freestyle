c2a9425c1df2c31251ce71230773867683d85371
subashpernandas <subashpstalin@gmail.com>
2025-09-25 16:47:42 +0530

    add freesyle project

---
+++ free-style/src/main/java/com/tech/subash/free_style/FreeStyleApplication.java
@@ -0,0 +1,13 @@
+package com.tech.subash.free_style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeStyleApplication.class, args);
+	}
+
+}
---
+++ free-style/src/test/java/com/tech/subash/free_style/FreeStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.subash.free_style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
